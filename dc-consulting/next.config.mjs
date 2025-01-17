c6bdde55aef69d5b13113873a4e0cf1be5e5094c
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2025-01-17 14:25:30 +0100

    Update Next.js configuration to include unoptimized images

--- dc-consulting/next.config.mjs
+++ dc-consulting/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Ensure you've specified the output as export
+  images: {
+    unoptimized: true, // Ensure images are not optimized
+  },
 };
 
 export default nextConfig;
