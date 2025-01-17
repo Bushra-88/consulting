22798425eca798c94b75c25ad02e70b02596ee03
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2025-01-17 13:23:51 +0100

    Update the Hero section

--- dc-consulting/components/Hero.jsx
+++ dc-consulting/components/Hero.jsx
@@ -1,15 +1,31 @@
 "use client";
-import Image
- from "next/image";
+import Image from "next/image";
+import { useEffect, useState } from "react"; // Import useEffect and useState
+
 const Hero = () => {
+  const [isVisible, setIsVisible] = useState(false); // State to control visibility
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true); // Set visibility to true after short delay
+    }, 200); // Adjust delay as needed
+    return () => clearTimeout(timer); // Cleanup timeout on unmount
+  }, []);
+
   return (
     <div className="relative">
-      <Image src="/hero.jpg" alt="Hero" 
-      width={1920}
-      height={500}
-      className="w-full h-auto object-cover" />
-      <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-c text-5xl text-customDark font-bold hover:text-sky-700 text-center">
-        Welkome bij Dilawarzada Consultatie
+      <Image 
+        src="/hero.jpg" 
+        alt="Hero" 
+        width={1920}
+        height={500}
+        className="w-full h-auto object-cover" 
+      />
+      <h1 
+        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold text-center transition-opacity duration-700 ${isVisible ? "opacity-100" : "opacity-0"}`}
+      >
+        Welkome bij <br />
+         <span className="text-sky-600 shadow-black">Dilawarzada Consultatie</span> 
       </h1>
     </div>
   );
