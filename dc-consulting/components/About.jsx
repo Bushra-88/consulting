bdfb62c5409370d54861a620839af32c3381b80c
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2025-01-17 12:40:36 +0100

    Create About component

---
+++ dc-consulting/components/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <section className="px-8 py-16 bg-gray-100">
+      <h2 className="text-3xl font-bold text-center mb-6">Over Ons</h2>
+      <p className="text-lg text-gray-700 max-w-2xl mx-auto text-center">
+        Ons consultatiebedrijf helpt mensen met advies op verschillende gebieden. 
+        We geven tips over onderwijs en studies, en helpen bij het kiezen van de 
+        juiste loopbaan. Ook ondersteunen we bij het starten van een eigen bedrijf 
+        en geven juridisch advies over arbeidsrecht. Daarnaast helpen we met 
+        toekomstplanning, zodat onze klanten klaar zijn voor wat er komt. Ons advies 
+        is altijd persoonlijk en afgestemd op de situatie van de klant. 
+        Voor meer informatie en afspraken kunt u ons contacteren via WhatsApp op   
+        <a href="https://wa.me/32487218610" target="_blank" rel="noopener noreferrer" className="text-green-600 font-bold">
+           +32487218610
+        </a> of via e-mail op  
+        <a href="mailto:Seyar.dilawar@gmail.com" className="text-blue-600 font-bold"> 
+         Seyar.dilawar@gmail.com.
+        </a>
+      </p>
+    </section>
+  );
+};
+
+export default About;
