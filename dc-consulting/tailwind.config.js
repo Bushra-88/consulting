e9489746679fbcc9510ce6bcf72e194ea7c2453a
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2025-01-17 12:39:15 +0100

    create custom color

--- dc-consulting/tailwind.config.js
+++ dc-consulting/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",      // if you're using the app directory
-    "./pages/**/*.{js,ts,jsx,tsx}",    // if you're using the pages directory
-    "./components/**/*.{js,ts,jsx,tsx}", // if you have a components directory
+    "./app/**/*.{js,ts,jsx,tsx}",     
+    "./pages/**/*.{js,ts,jsx,tsx}",    
+    "./components/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customDark: '#071417', 
+      },
+    },
   },
   plugins: [],
 };
