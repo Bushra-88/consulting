90ca4c9ef0a05ae2557886ff48280c541d5abf42
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2025-01-17 12:39:28 +0100

    Update Page.js

--- dc-consulting/app/page.js
+++ dc-consulting/app/page.js
@@ -1,13 +1,16 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <h1 className="text-4xl font-bold text-center mb-4">
-        Welcome to DC Consulting
-      </h1>
-      <p className="text-lg text-center">
-        ons consultatiebedrijf helpt mensen met advies op verschillende gebieden.
-        We geven tips over onderwijs en studies, en helpen bij het kiezen van de juiste loopbaan.
-      </p>
-    </div>
+
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Footer from "@/components/Footer";
+
+export default function Home(){
+  return(
+    <>
+      <Navbar />
+      <Hero/>
+      <About/>
+      <Footer/>
+    </>
   );
-}
+};
